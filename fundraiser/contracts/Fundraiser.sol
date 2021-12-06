d4d26e5d399724e48de734027e23215468311d5a
江トウ <>
2021-12-06 23:50:53 +0900

    no message

--- fundraiser/contracts/Fundraiser.sol
+++ fundraiser/contracts/Fundraiser.sol
@@ -1,15 +1,34 @@
-pragma solidity >= 0.4.22 <0.9.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
 
-contract Fundraiser {
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
+
+contract Fundraiser is Ownable {
   string public name;
   string public url;
   string public imageURL;
   string public description;
 
-  constructor(string memory _name, string memory _url, string memory _imageURL, string memory _description) public {
+  address payable public beneficiary;
+  address public custodian;
+
+  constructor(
+    string memory _name,
+    string memory _url,
+    string memory _imageURL,
+    string memory _description,
+    address payable _beneficiary,
+    address _custodian
+  ) {
     name = _name;
     url = _url;
     imageURL = _imageURL;
     description = _description;
+    beneficiary = _beneficiary;
+    transferOwnership(_custodian);
+  }
+
+  function setBeneficiary(address payable _beneficiary) public onlyOwner {
+    beneficiary = _beneficiary;
   }
 }
