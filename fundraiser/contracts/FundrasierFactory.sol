ffcec7befdb94fd18f1b210bb5cddc82ff0c4f18
江トウ <>
2021-12-16 22:22:34 +0900

    no message

--- fundraiser/contracts/FundrasierFactory.sol
+++ fundraiser/contracts/FundrasierFactory.sol
@@ -1,6 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract FundraiserFactory {
+import "./Fundraiser.sol";
 
+contract FundraiserFactory {
+  Fundraiser[] private _fundraisers;
+  event FundraiserCreated(Fundraiser indexed fundraiser, address indexed owner);
+
+  function fundraisersCount() public view returns(uint256) {
+    return _fundraisers.length;
+  }
+
+  function createFundrasier(
+    string memory name,
+    string memory url,
+    string memory imageURL,
+    string memory description,
+    address payable beneficiary
+  ) public {
+    Fundraiser fundraiser = new Fundraiser(
+      name,
+      url,
+      imageURL,
+      description,
+      beneficiary,
+      msg.sender
+    );
+    _fundraisers.push(fundraiser);
+    emit FundraiserCreated(fundraiser, fundraiser.owner());
+  }
 }
--- fundraiser/test/fundrasier_factory_test.js
+++ fundraiser/test/fundrasier_factory_test.js
@@ -6,3 +6,49 @@ contract("FundraiserFactory: deployment", () => {
     assert(fundraiserFactory, "fundraiser factory was not deployed");
   });
 });
+
+contract("FundraiserFactory: createFundrasier", (accounts) => {
+  let fundraiserFactory;
+  const name = "Beneficiary Name";
+  const url = "beneficiaryname.org";
+  const imageURL = "https://placekitten.com/600/350";
+  const description = "Beneficiary description";
+  const beneficiary = accounts[1];
+
+  it ("incrments the fundraisersCount", async () => {
+    fundraiserFactory = await FundraiserFactoryContract.deployed();
+    const currentFundraisersCount = await fundraiserFactory.fundraisersCount();
+    await fundraiserFactory.createFundrasier(
+      name,
+      url,
+      imageURL,
+      description,
+      beneficiary
+    );
+    const newFundraisersCount = await fundraiserFactory.fundraisersCount();
+
+    assert(
+      newFundraisersCount - currentFundraisersCount,
+      1,
+      "should incrment by 1"
+    );
+  });
+
+  it ("emit the FundraiserCreated event", async () => {
+    fundraiserFactory = await FundraiserFactoryContract.deployed();
+    const tx = await fundraiserFactory.createFundrasier(
+      name,
+      url,
+      imageURL,
+      description,
+      beneficiary
+    );
+    const expectedEvent = "FundraiserCreated";
+    const actualEvent = tx.logs[0].event;
+    assert.equal(
+      actualEvent,
+      expectedEvent,
+      "events should match"
+    )
+  });
+});
